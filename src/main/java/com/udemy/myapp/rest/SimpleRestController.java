709981311a52a08724c4f23ef75829de356f38a9
fuadze <fzein.1695@gmail.com>
2026-02-02 21:30:45 +0700

    section 1: 17 - 18. injectiong custom properties

--- src/main/java/com/udemy/myapp/rest/SimpleRestController.java
+++ src/main/java/com/udemy/myapp/rest/SimpleRestController.java
@@ -1,5 +1,6 @@
 package com.udemy.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,21 +8,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class SimpleRestController {
 
-  // expose "/" that returns "My Application"
+  // ? add properties for: instructor.name & student.class
+  @Value("${instructor.name}")
+  private String instructorName;
+
+  @Value("${student.class}")
+  private String studentClass;
+
+  // ? expose "/" that returns "My Application"
   @GetMapping("/")
   public String app() {
     return "My Application.";
   }
 
-  // expose new endpoint for learning
+  // ? expose new endpoint for learning
   @GetMapping("/learning")
   public String getLearningGoals() {
     return "Learn 4 modules each day!";
   }
 
-  // expose new endpoint for gaming
+  // ? expose new endpoint for gaming
   @GetMapping("/gaming")
   public String getDailyLevels() {
     return "Comlete 10 levels per day!";
+  }
+
+  // ? expose a new endpoint for classInfo
+  @GetMapping("/classinfo")
+  public String getClassInfo() {
+    return "Instructor: " + instructorName + ", Class: " + studentClass;
   }
 }
