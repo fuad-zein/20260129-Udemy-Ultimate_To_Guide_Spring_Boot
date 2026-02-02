c4ccf5a7bd2b72672b898b1365c13ee590795c1e
fuadze <fzein.1695@gmail.com>
2026-02-02 18:05:55 +0700

    section 1: 12. spring boot dev tools - coding

--- src/main/java/com/udemy/myapp/rest/SimpleRestController.java
+++ src/main/java/com/udemy/myapp/rest/SimpleRestController.java
@@ -12,4 +12,16 @@ public class SimpleRestController {
   public String app() {
     return "My Application.";
   }
+
+  // expose new endpoint for learning
+  @GetMapping("/learning")
+  public String getLearningGoals() {
+    return "Learn 4 modules each day!";
+  }
+
+  // expose new endpoint for gaming
+  @GetMapping("/gaming")
+  public String getDailyLevels() {
+    return "Comlete 10 levels per day!";
+  }
 }
